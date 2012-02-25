039b842ede3cc9a00c1a8cf0f85ba575e297c050
Sebastian Salinas P <sisalina@puc.cl>
2012-02-25 16:10:00 -0300

    ok

---
+++ Blink.c
@@ -0,0 +1,30 @@
+void main() {
+
+  DDRB.B0 = 1;
+  DDRB.B1 = 1;
+  DDRB.B2 = 1;
+  DDRB.B3 = 1;
+  DDRB.B4 = 1;
+  DDRB.B5 = 1;
+  
+  while(1){
+   PORTB.B0=1;
+   PORTB.B1=0;
+   PORTB.B2=1;
+   PORTB.B3=0;
+   PORTB.B4=1;
+   PORTB.B5=0;
+   
+   delay_ms(500);
+   
+   PORTB.B0=0;
+   PORTB.B1=1;
+   PORTB.B2=0;
+   PORTB.B3=1;
+   PORTB.B4=0;
+   PORTB.B5=1;
+
+   delay_ms(500);
+  
+  }
+}
